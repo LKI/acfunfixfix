aa8f95e87904cc74a0e8b8e8bd32c6477c8727fd
Lirian Su <liriansu@gmail.com>
2016-04-14 19:41:41 +0800

    Improve code and add change log
    * Fix bug "refresh-player" button will append endlessly.
    * In future commits I'm gonna change CRLF to LF.

--- js/acfunfixfix.js
+++ js/acfunfixfix.js
@@ -1,4 +1,4 @@
- (function() {
+(function() {
   if (!document.domain) {
     alert("你拖到书签栏使用了么？书签栏应该是在浏览器地址栏下方的位置。如果你确定你是在书签栏使用的，那么可能你的浏览器太辣鸡（比如360），换个现代的浏览器吧（火狐、谷歌、IE11都行的）");
     return
@@ -11,10 +11,10 @@
     alert("进AcFun，开个视频，再点我...");
     return
   }
-  $.info("AcFun Fix Fix: 欢迎使用 AcFun Fix Fix 2016-04-11 E-mail:liriansu@gmail.com");
-  var b = $("a.active.primary").data("from");
+  $.info("欢迎使用 AcFun Fix Fix 2016-4-14 E-mail:liriansu@gmail.com");
+  var source = $("a.active.primary").data("from");
   window._getPlayer = function() {
-    return document.getElementById("ACFlashPlayer-re") ? document.getElementById("ACFlashPlayer-re") : (document.getElementById("not-ACFlashPlayer-re") ? document.getElementById("not-ACFlashPlayer-re") : document.getElementById("area-player"));
+    return document.getElementById("ACFlashPlayer-re") || document.getElementById("not-ACFlashPlayer-re") || document.getElementById("area-player");
   };
   window.c = function(d, e) {
     player = _getPlayer();
@@ -24,14 +24,14 @@
     };
     player.outerHTML = '<object style="visibility:visible;width:100%;height:100%" id="not-ACFlashPlayer-re" data="' + d + '" src="' + d + '" allowscriptaccess="always" allowfullscreen="true" allowfullscreeninteractive="true" type="application/x-shockwave-flash"><param value="true" name="allowFullscreenInteractive"><param value="true" name="allowfullscreen"><param value="always" name="allowscriptaccess"><param value="' + e + '" name="flashvars"><param name=movie value="' + d + '"></object>'
   };
-  if (!document.getElementById("video-download") && b != "iqiyi" && b != "pps") {
+  if (!document.getElementById("video-download") && source != "iqiyi" && source != "pps") {
     $("#txt-title-view").append('<span id="video-download"><a class="btn primary" href="http://www.liriansu.com/acfunfixfix/danmaku.html?vid='+$("a.active.primary").data("vid")+'&p='+(location.href.match(/_(\d+)/)?location.href.match(/_(\d+)/)[1]:"1")+'" title="视频下载" style="float:none;color:#fff;margin-left:8px;" target="_blank"><i class="icon icon-download"></i>详细信息及下载</a></span>')
   }
-  if (b == "youku2") {
-    b = "youku"
+  if (source == "youku2") {
+    source = "youku"
   }
-  if (b == "qq2") {
-    b = "qq"
+  if (source == "qq2") {
+    source = "qq"
   }
   sourceList = {
     "sina": "新浪视频",
@@ -49,41 +49,26 @@
     "zhuzhan": "主站",
     "ac": "主站"
   };
-  if(typeof(sourceList[b]) == "undefined"){
+  if(typeof(sourceList[source]) == "undefined"){
     $.ajax({
       url: "http://www.acfun.tv/video/getVideo.aspx?id="+$("a.active.primary").data("vid"),
       async: false,
       success:function(data){
         $("a.active.primary").data("from",data.sourceType);
         $("a.active.primary").data("sid",data.sourceId);
-        b = data.sourceType;
+        source = data.sourceType;
       }
     });
   };
-  if (b != "letv" && b != "zhuzhan" && b != "ac") {
-    if(b == "iqiyi"||b == "pps"){
+  if (source != "letv" && source != "zhuzhan" && source != "ac") {
+    if(source == "iqiyi"||source == "pps"){
       $.info("由于一些版权方面的原因，爱奇艺源停止替换播放器。请各位理解。");
     }else{
-      c("http://static.skydust.net/private/acfun/AcPlayer201412121_D.swf", "oldcs=1&host=http://www.talkshowcn.com&vid=" + $("a.active.primary").data("vid") + "|" + b + "|" + $("a.active.primary").data("sid"));
-      $("#video-download").append('<a class="btn primary" onclick="$(_getPlayer()).prop(\'outerHTML\',$(_getPlayer()).prop(\'outerHTML\').replace(/acfun.tv/,\'talkshowcn.com\'))" style="float:none;color:#fff;margin-left:8px;" target="_blank"><i class="icon icon-refresh"></i>若解析失败点这儿刷新几次</a>');
-      $.info("视频源类型：" + sourceList[b]);
+      c("http://static.skydust.net/private/acfun/AcPlayer201412121_D.swf", "oldcs=1&host=http://www.talkshowcn.com&vid=" + $("a.active.primary").data("vid") + "|" + source + "|" + $("a.active.primary").data("sid"));
+      if (!document.getElementById("refresh-player")) {$("#video-download").append('<a id="refresh-player" class="btn primary" onclick="$(_getPlayer()).prop(\'outerHTML\',$(_getPlayer()).prop(\'outerHTML\'))" style="float:none;color:#fff;margin-left:8px;" target="_blank"><i class="icon icon-refresh"></i>解析失败请点我刷新</a>');};
+      $.info("视频源类型：" + sourceList[source]);
     }
   }else{
     $.info("本程序不会对 乐视云源和主站视频源 进行任何处理，出现问题是 AcFun 的问题，请联系客服。");
   }
-  window.setCookie = function(d, f) {
-    var e = 365;
-    var g = new Date();
-    g.setTime(g.getTime() + e * 24 * 60 * 60 * 1000);
-    document.cookie = d + "=" + escape(f) + ";expires=" + g.toGMTString()
-  };
-
-  function a(e) {
-    var d, f = new RegExp("(^| )" + e + "=([^;]*)(;|$)");
-    if (d = document.cookie.match(f)) {
-      return unescape(d[2])
-    } else {
-      return null
-    }
-  }
- })();
+})();
